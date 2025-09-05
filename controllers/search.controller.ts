316bb999d3d8d44f8e2f2ecd2bea898b54dec7f6
Tuệ Phạm <tuepham634@gmail.com>
2025-09-05 08:18:23 +0700

    Hiện thị dữ liệu theo các trường tìm kiếm và lọc

--- controllers/search.controller.ts
+++ controllers/search.controller.ts
@@ -5,57 +5,99 @@ import City from "../models/city.model";
 
 
 export const search = async (req: Request, res: Response) => {
-//   console.log(req.query.language);
-  const dataFinal = [];
+    //   console.log(req.query.language);
+    const dataFinal = [];
 
-  if(Object.keys(req.query).length > 0) {
-    const find: any = {};
+    if (Object.keys(req.query).length > 0) {
+        const find: any = {};
 
-    // Language
-    if(req.query.language) {
-      find.technologies = req.query.language;
+        // Language
+        if (req.query.language) {
+            find.technologies = req.query.language;
+        }
+        //city
+        if (req.query.city) {
+            const city = await City.findOne({
+                name: req.query.city
+            })
+            if (city) {
+                const listAccountCompanyInCity = await AccountCompany.find({
+                    city: city._id
+                })
+                const listIdAccountCompany = listAccountCompanyInCity.map(item => item.id);
+
+                find.companyId = { $in: listIdAccountCompany };
+
+            }
+        }
+        //company
+        if(req.query.company){
+            const company = await AccountCompany.findOne({
+                companyName: req.query.company
+            })
+            if(company){
+                find.companyId = company?.id;
+            }
+        }
+
+        // Keyword
+        if(req.query.keyword) {
+        const keywordRegex = new RegExp(`${req.query.keyword}`, "i");
+        find["$or"] = [
+            { title: keywordRegex },
+            { technologies: keywordRegex }
+        ];
+        }
+        //position
+        if(req.query.position) {
+            find.position = req.query.position;
+
+        }
+        //workingForm
+        if(req.query.workingForm) {
+            find.workingForm = req.query.workingForm;
+        }
+
+        const jobs = await Job
+            .find(find)
+            .sort({
+                createdAt: "desc"
+            })
+
+        for (const item of jobs) {
+            const itemFinal = {
+                id: item.id,
+                companyLogo: "",
+                title: item.title,
+                companyName: "",
+                salaryMin: item.salaryMin,
+                salaryMax: item.salaryMax,
+                position: item.position,
+                workingForm: item.workingForm,
+                companyCity: "",
+                technologies: item.technologies,
+            };
+
+            const companyInfo = await AccountCompany.findOne({
+                _id: item.companyId
+            })
+
+            if (companyInfo) {
+                itemFinal.companyLogo = `${companyInfo.logo}`;
+                itemFinal.companyName = `${companyInfo.companyName}`;
+
+                const city = await City.findOne({
+                    _id: companyInfo.city
+                })
+                itemFinal.companyCity = `${city?.name}`;
+            }
+
+            dataFinal.push(itemFinal);
+        }
     }
-
-    const jobs = await Job
-      .find(find)
-      .sort({
-        createdAt: "desc"
-      })
-    
-    for (const item of jobs) {
-      const itemFinal = {
-        id: item.id,
-        companyLogo: "",
-        title: item.title,
-        companyName: "",
-        salaryMin: item.salaryMin,
-        salaryMax: item.salaryMax,
-        position: item.position,
-        workingForm: item.workingForm,
-        companyCity: "",
-        technologies: item.technologies,
-      };
-
-      const companyInfo = await AccountCompany.findOne({
-        _id: item.companyId
-      })
-
-      if(companyInfo) {
-        itemFinal.companyLogo = `${companyInfo.logo}`;
-        itemFinal.companyName = `${companyInfo.companyName}`;
-        
-        const city = await City.findOne({
-          _id: companyInfo.city
-        })
-        itemFinal.companyCity = `${city?.name}`;
-      }
-
-      dataFinal.push(itemFinal);
-    }
-  }
-  res.json({
-    code: "success",
-    message: "Thành công!",
-    jobs: dataFinal
-  })
+    res.json({
+        code: "success",
+        message: "Thành công!",
+        jobs: dataFinal
+    })
 }
